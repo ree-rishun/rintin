d92fceef99165ebe91a2ebb5fb8dbef98d421f19
ree-rishun <42071202+ree-rishun@users.noreply.github.com>
2020-05-30 14:29:51 +0900

    incam test2

--- public/javascripts/ocr.js
+++ public/javascripts/ocr.js
@@ -1,10 +1,16 @@
-const medias = {audio : false, video : true};
-
+const medias = {
+    audio: false,
+    video: {
+        facingMode: {
+            exact: "environment"
+        }
+    }
+};
 const video = document.getElementById("video");
 const promise = navigator.mediaDevices.getUserMedia(medias);
 
 promise.then(successCallback)
-    .then(errorCallback);
+    .catch(errorCallback);
 
 function successCallback(stream) {
     video.srcObject = stream;
