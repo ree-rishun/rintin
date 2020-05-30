0bed461632949040da57a1bb401d806ca403c9b6
ree-rishun <42071202+ree-rishun@users.noreply.github.com>
2020-05-30 14:26:36 +0900

    incam test

--- public/javascripts/ocr.js
+++ public/javascripts/ocr.js
@@ -1,9 +1,4 @@
-const medias = {audio : false, video : {
-        facingMode : {
-            exact : "environment"
-        }
-    }
-};
+const medias = {audio : false, video : true};
 
 const video = document.getElementById("video");
 const promise = navigator.mediaDevices.getUserMedia(medias);
