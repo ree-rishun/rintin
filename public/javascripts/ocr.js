687e038b79910db2dc3386c13f13903f1c24bd82
ree-rishun <42071202+ree-rishun@users.noreply.github.com>
2020-05-31 13:40:38 +0900

    add menu

--- public/javascripts/ocr.js
+++ public/javascripts/ocr.js
@@ -4,9 +4,10 @@ let recognition_lang = 'eng';
 
 function convert_text(url) {
     console.log(url);
+    let selectBox = document.getElementById("language");
     Tesseract
         .recognize(url, {
-            lang: recognition_lang
+            lang: selectBox.options[selectBox.selectedIndex].value
         })
         .then(function(result) {
             console.log(result);
@@ -16,3 +17,19 @@ function convert_text(url) {
         qrcode_reader();
     }, 500);
 }
+
+// 言語の切り替え
+let nav_display = false;
+let nav_button = document.getElementById("nav_button");
+let menu_area = document.getElementById("menu");
+
+// ナビゲーションの表示
+nav_button.addEventListener('touchend', function(e) {
+    console.log(nav_display);
+    if(nav_display){
+        menu_area.style.display="none";
+    }else{
+        menu_area.style.display="inline-block";
+    }
+    nav_display = !nav_display;
+});
