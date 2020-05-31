4ba7716797b156f411686d727ad53f6ae61ae704
ree-rishun <42071202+ree-rishun@users.noreply.github.com>
2020-05-31 10:26:31 +0900

    mode english

--- public/javascripts/ocr.js
+++ public/javascripts/ocr.js
@@ -1,10 +1,12 @@
 let print = document.getElementById( "select_square" );
 
+let recognition_lang = 'eng';
+
 function convert_text(url) {
     console.log(url);
     Tesseract
         .recognize(url, {
-            lang: 'jpn'
+            lang: recognition_lang
         })
         .then(function(result) {
             console.log(result);
